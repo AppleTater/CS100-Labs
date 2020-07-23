98d79eeb5cddb6f1cf8e8f1eca22206c5a985c64
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-07-22 23:22:24 -0700

    Create op.hpp

---
+++ lab-05-decorator-pattern-sigsegv-noises/op.hpp
@@ -0,0 +1,22 @@
+  
+#ifndef __OP_HPP__
+#define __OP_HPP__
+
+#include "base.hpp"
+#include <string>
+
+using namespace std;
+class Op : public Base {
+	private:
+		double val;
+	public:
+		Op(double value) : Base() { val = value; }
+		virtual double evaluate() { return val; }
+		virtual std::string stringify() { 
+			int toString = static_cast<int>(val);
+			string output = to_string(toString);
+			return output;
+		 }
+};
+
+#endif //__OP_HPP__v
