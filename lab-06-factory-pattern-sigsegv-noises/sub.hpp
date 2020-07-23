da16201317ab2fa51aa2430bd6efa15a3c068b89
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-07-22 23:28:35 -0700

    Create sub.hpp

---
+++ lab-06-factory-pattern-sigsegv-noises/sub.hpp
@@ -0,0 +1,30 @@
+  
+#ifndef _SUB_HPP_
+#define _SUB_HPP_
+
+#include "op.hpp"
+
+using namespace std; 
+
+class Sub : public Base {
+	public:
+		Sub(Base* left, Base* right) : Base() {
+			lstr = left->stringify();
+			rstr = right->stringify();
+			ldbl = left -> evaluate();
+			rdbl = right -> evaluate();
+		}
+		virtual double evaluate() {
+			return(ldbl - rdbl);
+		}
+		virtual string stringify() {
+			return(lstr + " - " + rstr);
+		}
+	private:
+		string lstr;
+		string rstr;
+		double ldbl; 
+		double rdbl; 
+};
+
+#endif
